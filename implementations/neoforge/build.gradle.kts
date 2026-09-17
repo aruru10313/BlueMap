33871e451c7f15543cf7e89cf49e36d3a0305eda
aruru10313 <aruru10313@users.noreply.github.com>
2026-09-17 09:44:47 +0900

    Fix release task inputFile property binding to mergeShadowAndJarJar.archiveFile

--- implementations/neoforge/build.gradle.kts
+++ implementations/neoforge/build.gradle.kts
@@ -99,7 +99,7 @@ val mergeShadowAndJarJar = tasks.create<Jar>("mergeShadowAndJarJar") {
 
 tasks.getByName<CopyFileTask>("release") {
     dependsOn( mergeShadowAndJarJar )
-    inputFile.set(mergeShadowAndJarJar.flatMap { it.archiveFile })
+    inputFile.set(mergeShadowAndJarJar.archiveFile)
 }
 
 modrinth {
